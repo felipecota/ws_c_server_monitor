e7b44e19580c95f51273966a78a5650286a3201f
Felipe Cota <felipe_cota@hotmail.com>
2017-09-06 16:50:04 -0300

    Update free.cpp

--- free.cpp
+++ free.cpp
@@ -5,14 +5,16 @@
 // Using perl script (free.pl) from https://github.com/famzah/linux-memavailable-procfs/blob/master/Linux-MemAvailable.pm
 
 int getRAMAvailable() {
+    
     FILE* fp = popen("./free.pl", "r");
+    
     if(fp) {
+        
         char buf[200];
         unsigned long int kbytes;
 
         while (fgets(buf, 200, fp)) {
-            sscanf(buf, " -/+ avail %*d %lu",
-            &kbytes);
+            sscanf(buf, " -/+ avail %*d %lu", &kbytes);
         }
 
         pclose(fp);
